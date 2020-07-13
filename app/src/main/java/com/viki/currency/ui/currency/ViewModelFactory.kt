2a60d170d3ea56b8a5b197888c0736f8f62940b4
Manuv <manuram692@gmail.com>
2020-07-14 02:26:15 +0800

    Code refactoring Adding comments

--- app/src/main/java/com/viki/currency/ui/currency/ViewModelFactory.kt
+++ app/src/main/java/com/viki/currency/ui/currency/ViewModelFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.viki.currency.repository.CurrencyRepository
 
+//Since we are not using Dagger or anything - I'm passing the repository instance via Viewmodel factory.
 class ViewModelFactory (private val currencyRepository: CurrencyRepository) : ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
