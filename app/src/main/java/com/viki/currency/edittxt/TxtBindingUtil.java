b4f13e874ad990de3e2142a593698ec5c76f453d
Manuv <manuram692@gmail.com>
2020-07-13 21:53:24 +0800

    Fixing the Tocurrency feld NAN by modifying the adapter

--- app/src/main/java/com/viki/currency/edittxt/TxtBindingUtil.java
+++ app/src/main/java/com/viki/currency/edittxt/TxtBindingUtil.java
@@ -1,54 +1,12 @@
 package com.viki.currency.edittxt;
 
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.databinding.BindingAdapter;
 import androidx.databinding.InverseBindingAdapter;
 
 public class TxtBindingUtil {
-   /* @BindingAdapter("android:text")
-    public static void setFloat(TextView view, float value) {
-        if (Float.isNaN(value)) view.setText("");
-        else view.setHint("value");
-    }
-
-    @InverseBindingAdapter(attribute = "android:text")
-    public static float getFloat(TextView view) {
-        String num = view.getText().toString();
-        if(num.isEmpty()) return 0.0F;
-        try {
-            return Float.parseFloat(num);
-        } catch (NumberFormatException e) {
-            return 0.0F;
-        }
-    }*/
-
-
-   /* @BindingAdapter("android:text")
-    public static void setText(TextView view, float value) {
-        boolean setValue = view.getText().length() == 0;
-        try {
-            if (!setValue) {
-                setValue = Float.parseFloat(view.getText().toString()) != value;
-            }
-        } catch (NumberFormatException e) {
-            view.setHint("enter manu");
-        }
-        if (setValue) {
-            view.setText(String.valueOf(value));
-        }
-    }
-
-    @InverseBindingAdapter(attribute = "android:text")
-    public static Object getText(TextView view) {
-        try {
-            return Float.parseFloat(view.getText().toString());
-        } catch (NumberFormatException e) {
-            view.setHint("wrong");
-            return null;
-        }
-    }*/
-
     @BindingAdapter("android:text")
     public static void setDouble(TextView view, Double val) {
         if (val != null) {
@@ -70,9 +28,17 @@ public class TxtBindingUtil {
     @InverseBindingAdapter(attribute = "android:text")
     public static Double getDouble(TextView view) {
         try {
-            return Double.parseDouble(view.getText().toString());
+            if(view.getText().toString().equals("NaN")){
+                view.setHint("Enter Number");
+                view.setText("");
+                return null;
+            } else {
+                return Double.parseDouble(view.getText().toString());
+            }
         } catch (NumberFormatException e) {
+            view.setHint("Enter Number");
             return null;
         }
     }
 }
+
--- app/src/main/java/com/viki/currency/ui/currency/CurrencyConvertViewModel.kt
+++ app/src/main/java/com/viki/currency/ui/currency/CurrencyConvertViewModel.kt
@@ -1,6 +1,7 @@
 package com.viki.currency.ui.currency
 
 import android.os.Looper
+import android.text.TextUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -38,12 +39,12 @@ class CurrencyConvertViewModel(val currencyRepository:CurrencyRepository) :ViewM
 
 
 
-    var dataFromSever : MutableLiveData<JsonElement>
-    private var todoLiveData: MutableLiveData<CurrencyApiResult>
-
     private var _errorLiveData= MutableLiveData<String>()
     val errorLiveData: LiveData<String>
         get() = _errorLiveData
+
+    //display the conversionrate
+    var displayConversionRate  = MutableLiveData<String>()
 
 
 
@@ -53,9 +54,8 @@ class CurrencyConvertViewModel(val currencyRepository:CurrencyRepository) :ViewM
         curToSpnrData  = MutableLiveData<MutableList<CurrencyData>> ()
         spinnerFromSelectedPosition = MutableLiveData<Int>()
         spinnerToSelectedPosition = MutableLiveData<Int>()
+        //displayConversionRate = MutableLiveData<String>()
 
-        dataFromSever =  MutableLiveData<JsonElement>()
-        todoLiveData =  MutableLiveData<CurrencyApiResult>()
 
 
     }
@@ -124,23 +124,39 @@ class CurrencyConvertViewModel(val currencyRepository:CurrencyRepository) :ViewM
 
         val fromSpinnerSelectedItem = curDataFromApi.get(spinnerFromSelectedPosition.value ?:0).value
         val toSpinnerSelectedItem = curDataFromApi.get(spinnerToSelectedPosition.value ?: 0).value
+        if(fromCurrencyEdtLiveData?.value == null) {
+            toCurrencyEdtLiveData.postValue(Double.NaN)
+            Log.d(TAG,"setting tovalue to null::")
+        }
+        Log.d(TAG,"setting tovalue fromCurrencyEdtLiveData to null::"+fromCurrencyEdtLiveData?.value)
+
 
         val fromFieldValue = fromCurrencyEdtLiveData.value
 
+        fromCurrencyEdtLiveData?.value?.let {
+            Log.d(TAG,"Onselcting from dropdown..fromspnr="+fromSpinnerSelectedItem+"..tospnr== "+toSpinnerSelectedItem+"..fromEdittextvalue=="+fromFieldValue+"..toCurrencyLiveData="+toCurrencyEdtLiveData.value)
 
-        Log.d(TAG,"Onselcting from dropdown..fromspnr="+fromSpinnerSelectedItem+"..tospnr== "+toSpinnerSelectedItem+"..fromEdittextvalue=="+fromFieldValue+"..toCurrencyLiveData="+toCurrencyEdtLiveData.value)
+            val  calculateSum = (fromCurrencyEdtLiveData.value?.times(fromSpinnerSelectedItem.toString().toDouble()))?.plus(toSpinnerSelectedItem.toString().toDouble())
 
-       val  calculateSum = (fromCurrencyEdtLiveData.value?.times(fromSpinnerSelectedItem.toString().toDouble()))?.plus(toSpinnerSelectedItem.toString().toDouble())
+            val currencyDiff = toSpinnerSelectedItem.toString().toDouble().div(fromSpinnerSelectedItem.toString().toDouble())
 
-        val currencyDiff = toSpinnerSelectedItem.toString().toDouble().div(fromSpinnerSelectedItem.toString().toDouble())
+            val finalSum = fromFieldValue?.times(currencyDiff)
 
-        val finalSum = fromFieldValue?.times(currencyDiff)
+            toCurrencyEdtLiveData.value = finalSum
+        }
 
-        toCurrencyEdtLiveData.value = finalSum
+
+
+        displayConversionRate.value = "1 " +  curDataFromApi.get(spinnerFromSelectedPosition.value ?:0).name +" equals "+
+                curDataFromApi.get(spinnerToSelectedPosition.value ?: 0).value +" " +curDataFromApi.get(spinnerToSelectedPosition.value ?: 0).name
 
 
     }
 
+
+    fun displayRate(){
+
+    }
 
 
 
